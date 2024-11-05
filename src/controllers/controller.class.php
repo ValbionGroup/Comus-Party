c416f29155e01e7b282889705fdefcc06d3b5959
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-11-05 11:00:26 +0100

    :memo: Documentation des classes Controller et Db

--- src/controllers/controller.class.php
+++ src/controllers/controller.class.php
@@ -1,15 +1,53 @@
 <?php
 
+/**
+ * La classe Controller est la classe mère de tous les contrôleurs
+ */
 class Controller {
+    /**
+     * La connexion à la base de données
+     *
+     * @var PDO
+     */
     private PDO $pdo;
+
+    /**
+     * Le loader de Twig
+     *
+     * @var \Twig\Loader\FilesystemLoader
+     */
     private \Twig\Loader\FilesystemLoader $loader;
+
+    /**
+     * L'environnement de Twig
+     *
+     * @var \Twig\Environment
+     */
+
     private \Twig\Environment $twig;
+    /**
+     * Les données GET
+     *
+     * @var array|null
+     */
     private ?array $get = null;
+
+    /**
+     * Les données POST
+     *
+     * @var array|null
+     */
     private ?array $post = null;
 
+    /**
+     * Constructeur de la classe Controller
+     *
+     * @param \Twig\Loader\FilesystemLoader $loader
+     * @param \Twig\Environment $twig
+     */
     public function __construct(\Twig\Loader\FilesystemLoader $loader, \Twig\Environment $twig) {
-        $db = Bd::getInstance();
-        $this->pdo = Bd::getInstance()->getConnection();
+        $db = Db::getInstance();
+        $this->pdo = Db::getInstance()->getConnection();
         $this->loader = $loader;
         $this->twig = $twig;
 
@@ -22,6 +60,12 @@ class Controller {
         }
     }
 
+    /**
+     * Appelle une méthode du contrôleur fournie en paramètre
+     *
+     * @param string $method
+     * @return mixed
+     */
     public function call(string $method) : mixed {
         if (!method_exists($this, $method)) {
             return $this->error('Method not found in controller'. __CLASS__);
@@ -29,51 +73,106 @@ class Controller {
         return $this->$method();
     }
 
+    /**
+     * Retourne la connexion à la base de données
+     *
+     * @return PDO
+     */
     public function getPdo(): PDO
     {
         return $this->pdo;
     }
 
+    /**
+     * Modifie la connexion à la base de données
+     *
+     * @param PDO $pdo
+     * @return void
+     */
     public function setPdo(PDO $pdo): void
     {
         $this->pdo = $pdo;
     }
 
+    /**
+     * Retourne le loader de Twig
+     *
+     * @return \Twig\Loader\FilesystemLoader
+     */
     public function getLoader(): \Twig\Loader\FilesystemLoader
     {
         return $this->loader;
     }
 
+    /**
+     * Modifie le loader de Twig
+     *
+     * @param \Twig\Loader\FilesystemLoader $loader
+     * @return void
+     */
     public function setLoader(\Twig\Loader\FilesystemLoader $loader): void
     {
         $this->loader = $loader;
     }
 
+    /**
+     * Retourne l'environnement de Twig
+     *
+     * @return \Twig\Environment
+     */
     public function getTwig(): \Twig\Environment
     {
         return $this->twig;
     }
 
+    /**
+     * Modifie l'environnement de Twig
+     *
+     * @param \Twig\Environment $twig
+     * @return void
+     */
     public function setTwig(\Twig\Environment $twig): void
     {
         $this->twig = $twig;
     }
 
+    /**
+     * Retourne les données GET
+     *
+     * @return array|null
+     */
     public function getGet(): ?array
     {
         return $this->get;
     }
 
+    /**
+     * Modifie les données GET
+     *
+     * @param array|null $get
+     * @return void
+     */
     public function setGet(?array $get): void
     {
         $this->get = $get;
     }
 
+    /**
+     * Retourne les données POST
+     *
+     * @return array|null
+     */
     public function getPost(): ?array
     {
         return $this->post;
     }
 
+    /**
+     * Modifie les données POST
+     *
+     * @param array|null $post
+     * @return void
+     */
     public function setPost(?array $post): void
     {
         $this->post = $post;
---
+++ src/models/db.class.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * La classe Bd est un singleton qui permet de se connecter à la base de données
+ */
+class Db {
+    /**
+     * Instance du singleton de la base de données
+     *
+     * @var Db|null
+     */
+    private static ?Db $instance = null;
+
+    /**
+     * Classe PDO pour la connexion à la base de données
+     *
+     * @var PDO|null
+     */
+    private ?PDO $pdo;
+
+    /**
+     * Contructeur de la classe Bd
+     */
+    private function __construct() {
+        try {
+            $this->pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+    }
+
+    /**
+     * Retourne l'instance de la classe Bd
+     *
+     * @return Db
+     */
+    public static function getInstance(): Db {
+        if (self::$instance == null) {
+            self::$instance = new Db();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * Retourne la connexion à la base de données
+     *
+     * @return PDO
+     */
+    public function getConnection(): PDO {
+        return $this->pdo;
+    }
+
+    /**
+     * Surcharge de la méthode __clone pour empêcher le clonage de l'objet
+     *
+     * @return void
+     */
+    private function __clone() {}
+
+    /**
+     * Surcharge de la méthode __wakeup pour empêcher la désérialisation de l'objet
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    private function __wakeup() {
+        throw new Exception("Cannot unserialize a singleton.");
+    }
+}
