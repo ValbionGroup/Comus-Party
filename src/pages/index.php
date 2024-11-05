9236f62c83db235ec94451d7b0ce889c8df668d6
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2024-11-05 14:59:48 +0100

    :bricks: Ajout de la gestion des controleurs dans index.php

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,6 +1,27 @@
 <?php
 require_once  '../../include.php';
+try {
+    $controller = $_GET["controller"] ?? '';
+    $methode = $_GET["method"] ?? '';
 
-$template = $twig->load('default.twig');
+    if($controller == '' && $methode == ''){
+        $controller = 'accueil';
+        $methode = 'afficher';
+    }
+
+    if($controller == ''){
+        throw new Exception("Le controleur n'est pas défini");
+    }
+
+    if($methode == ''){
+        throw new Exception("La méthode n'est pas défini");
+    }
+}catch (Exception $e){
+    die ($e->getMessage());
+}
+
+
+
+$template = $twig->load('accueil.twig');
 
 echo $template->render();
