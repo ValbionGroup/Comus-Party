a7c7d10b6dd92426c11cea7baa78ecb53d723688
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-11-05 17:36:19 +0100

    :truck: Ajout des inclusions des controllers

--- include.php
+++ include.php
@@ -2,7 +2,15 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-//require_once __DIR__ . '/config/const.php';
-//require_once __DIR__ . '/config/db.php';
+require_once __DIR__ . '/config/const.php';
+require_once __DIR__ . '/config/db.php';
 
 require_once __DIR__ . '/config/twig.php';
+
+require_once __DIR__ . '/src/controllers/controllerFactory.class.php';
+require_once __DIR__ . '/src/controllers/controller.class.php';
+require_once __DIR__ . '/src/controllers/ControllerProfile.class.php';
+
+require_once __DIR__ . '/src/models/db.class.php';
+require_once __DIR__ . '/src/models/player.class.php';
+require_once __DIR__ . '/src/models/player.dao.php';
