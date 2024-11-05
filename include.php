e94620f4de4714c1d1fd3693fbd230c557a31787
Lucas ESPIET <lespiet@iutbayonne.univ-pau.fr>
2024-11-05 16:59:38 +0100

    :ambulance: Ajout de l'initialisation de Dotenv

--- include.php
+++ include.php
@@ -2,7 +2,11 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-//require_once __DIR__ . '/config/const.php';
-//require_once __DIR__ . '/config/db.php';
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+require_once __DIR__ . '/config/const.php';
+require_once __DIR__ . '/config/db.php';
 
 require_once __DIR__ . '/config/twig.php';
