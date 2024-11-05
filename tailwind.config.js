76fb321dcd980bc303d406924a2d00615cae65a6
Lucas E. <espiet.l@valbion.com>
2024-11-05 19:52:08 +0100

    :lipstick: Ajouts des couleurs de la charte graphique

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,html,php,twig}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-violet': '#8338EC',
+        'rose': '#FF006E',
+        'celestial-blue': '#33A1FD',
+        'hot-magenta': '#FFEC51',
+        'maize': '#FFEC51',
+        'lavender-blush': '#F7EDF0',
+        'night': '#171614'
+      }
+    },
   },
   plugins: [],
 }
