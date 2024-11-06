c374b75264d70fdcb1c3cbe4834ce8459c8b8fce
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-11-06 15:27:00 +0100

    :art: Modification de la classe ControllerProfile en ControllerPlayer

--- src/controllers/controllerPlayer.class.php
+++ src/controllers/controllerPlayer.class.php
@@ -1,14 +1,14 @@
 <?php
 
-class ControllerProfile extends Controller {
+class ControllerPlayer extends Controller {
     public function __construct(\Twig\Loader\FilesystemLoader $loader, \Twig\Environment $twig) {
         parent::__construct($loader, $twig);
     }
 
-    public function afficher() {
+    public function viewByPlayer() {
         $player_uuid = $_GET['uuid'];
         $playerManager = new PlayerDAO($this->getPdo());
-        $player = $playerManager->findByUuid($player_uuid);
+        $player = $playerManager->findWithDetailByUuid($player_uuid);
         $template = $this->getTwig()->load('profil.twig');
         echo $template->render(array(
             "player" => $player
