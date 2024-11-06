aa45e048560099490cd72d86f44dc90a62f05136
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-11-06 15:30:22 +0100

    :sparkles: Ajout des inclusions des controllers

--- include.php
+++ include.php
@@ -10,3 +10,11 @@ require_once __DIR__ . '/config/const.php';
 require_once __DIR__ . '/config/db.php';
 
 require_once __DIR__ . '/config/twig.php';
+
+require_once __DIR__ . '/src/controllers/controllerFactory.class.php';
+require_once __DIR__ . '/src/controllers/controller.class.php';
+require_once __DIR__ . '/src/controllers/controllerPlayer.class.php';
+
+require_once __DIR__ . '/src/models/db.class.php';
+require_once __DIR__ . '/src/models/player.class.php';
+require_once __DIR__ . '/src/models/player.dao.php';
