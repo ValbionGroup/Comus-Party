2d51cc06cd8d7c4249e5c3a22b987b88df3c4b06
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-11-06 19:43:00 +0100

    :pencil2: Correction du nom du ControllerPlayer et gestion d'exceptions

--- src/controllers/controllerPlayer.class.php
+++ src/controllers/controllerPlayer.class.php
@@ -1,15 +1,23 @@
 <?php
 
-class ControllerProfile extends Controller {
+class ControllerPlayer extends Controller {
     public function __construct(\Twig\Loader\FilesystemLoader $loader, \Twig\Environment $twig) {
         parent::__construct($loader, $twig);
     }
 
     public function afficher() {
+        if (!isset($_GET['uuid'])) {
+            header('Location: /404');
+            exit();
+        }
         $player_uuid = $_GET['uuid'];
         $playerManager = new PlayerDAO($this->getPdo());
         $userManager = new UserDAO($this->getPdo());
         $player = $playerManager->findWithDetailByUuid($player_uuid);
+        if ($player === null) {
+            header('Location: /404');
+            exit();
+        }
         $user = $userManager->findById($player->getUserId());
         $template = $this->getTwig()->load('profil.twig');
         echo $template->render(array(
