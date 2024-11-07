9997e22faaca4c77f1cc5d39d46809bf60bc7737
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2024-11-07 09:33:02 +0100

    :bricks: Implementation de la classe ControllerGame

--- src/controllers/controllerGame.class.php
+++ src/controllers/controllerGame.class.php
@@ -1,6 +1,20 @@
 <?php
-
-class ControllerGame
+/**
+ * La classe ControllerGame permet de faire le lien entre la vue et l'objet Game
+ */
+class ControllerGame extends Controller
 {
+    public function __construct(\Twig\Loader\FilesystemLoader $loader, \Twig\Environment $twig)
+    {
+        parent::__construct($loader, $twig);
+    }
 
+    public function afficher(){
+        $gameManager = new GameDAO($this->getPdo());
+        $games = $gameManager->findAll();
+        $template = $this->getTwig()->load('accueil.twig');
+        echo $template->render(array(
+            "games" => $games
+        ));
+    }
 }
