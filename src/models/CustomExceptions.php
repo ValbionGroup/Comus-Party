e2aad63d89af7bca01b6659dcee509535d1ef968
Lucas E. <espiet.l@valbion.com>
2024-11-07 15:33:05 +0100

    :alembic: Ajout d'une exception pour un controller non trouvé

--- src/models/CustomExceptions.php
+++ src/models/CustomExceptions.php
@@ -6,7 +6,6 @@ use Exception;
 use Throwable;
 
 class RouteNotFoundException extends Exception {
-
     public function __construct($message, $code = 0, Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
     }
@@ -15,4 +14,14 @@ class RouteNotFoundException extends Exception {
         return __CLASS__ . ": [$this->code]: $this->message\n";
     }
 
+}
+
+class ControllerNotFoundException extends Exception {
+        public function __construct($message, $code = 0, Throwable $previous = null) {
+            parent::__construct($message, $code, $previous);
+        }
+
+        public function __toString() {
+            return __CLASS__ . ": [$this->code]: $this->message\n";
+        }
 }
