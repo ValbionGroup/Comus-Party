8f9315ea04e95309e8f87ecffff36f5a8dd0ce47
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2024-11-07 09:35:09 +0100

    :bricks: Ajout de la classe controllerGame dans include.php

--- include.php
+++ include.php
@@ -14,6 +14,7 @@ require_once __DIR__ . '/config/twig.php';
 require_once __DIR__ . '/src/controllers/controllerFactory.class.php';
 require_once __DIR__ . '/src/controllers/controller.class.php';
 require_once __DIR__ . '/src/controllers/controllerPlayer.class.php';
+require_once __DIR__ . '/src/controllers/controllerGame.class.php';
 
 require_once __DIR__ . '/src/models/db.class.php';
 require_once __DIR__ . '/src/models/player.class.php';
