9a54c021fcb858251a8399f13783c861110d5ad4
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-11-08 10:12:38 +0100

    :sparkles: Ajout des routes de connexion

--- src/routes.php
+++ src/routes.php
@@ -13,3 +13,17 @@ $router->get('/play/:slug', function ($slug) {
     echo 'Liste des jeux : ' . $slug;
     exit;
 });
+
+// Route pour afficher le formulaire de connexion
+$router->get('/login', function () use ($loader, $twig) {
+    $controller = new ControllerAuth($loader, $twig);
+    $controller->showLoginPage();
+    exit;
+});
+
+// Route pour traiter la soumission du formulaire de connexion
+$router->post('/login', function () use ($loader, $twig) {
+    $controller = new ControllerAuth($loader, $twig);
+    $controller->authenticate($_POST['email'], $_POST['password']);
+    exit;
+});
