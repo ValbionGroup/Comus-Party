ef2dae2aa0f0d365de9279ce6418251d64bf3e6e
TistouCode <soldarmetv@gmail.com>
2024-11-12 12:16:42 +0100

    :memo: Ajout de commentaire sur article.class.php

--- include.php
+++ include.php
@@ -8,18 +8,22 @@ $dotenv->load();
 
 require_once __DIR__ . '/config/const.php';
 require_once __DIR__ . '/config/db.php';
-
 require_once __DIR__ . '/config/twig.php';
+
+require_once __DIR__ . '/src/pages/errors.php';
+require_once __DIR__ . '/src/models/Router.php';
+require_once __DIR__ . '/src/models/CustomExceptions.php';
 
 require_once __DIR__ . '/src/models/db.class.php';
 require_once __DIR__ . '/src/models/player.class.php';
 require_once __DIR__ . '/src/models/player.dao.php';
-require_once __DIR__ . '/src/models/article.class.php';
-require_once __DIR__ . '/src/models/article.dao.php';
-
+require_once __DIR__ . '/src/models/user.class.php';
+require_once __DIR__ . '/src/models/user.dao.php';
+require_once __DIR__ . '/src/models/game.class.php';
+require_once __DIR__ . '/src/models/game.dao.php';
+require_once __DIR__ . '/src/models/statistics.class.php';
 
 require_once __DIR__ . '/src/controllers/controllerFactory.class.php';
 require_once __DIR__ . '/src/controllers/controller.class.php';
 require_once __DIR__ . '/src/controllers/controllerProfile.class.php';
-require_once __DIR__ . '/src/controllers/controllerShop.class.php';
-
+require_once __DIR__ . '/src/controllers/controllerGame.class.php';
--- src/models/article.class.php
+++ src/models/article.class.php
@@ -1,6 +1,9 @@
 <?php
 // Type enum pour avatar et banniere
 
+/**
+ *
+ */
 enum Type{
     case ProfilePicture;
     case Banner;
@@ -8,21 +11,52 @@ enum Type{
 
 
 // Objet représentant un Article
+
+/**
+ *
+ */
 class Article {
 
 
+    /**
+     * @var int|null
+     */
     private ?int $id;
 
+    /**
+     * @var DateTime|null
+     */
     private ?DateTime $createdAt;
 
+    /**
+     * @var DateTime|null
+     */
     private ?DateTime $updatedAt;
+    /**
+     * @var string|null
+     */
     private ?string $name;
 
+    /**
+     * @var Type|null
+     */
     private ?Type $type;
 
+    /**
+     * @var string|null
+     */
     private ?string $description;
+    /**
+     * @var int|null
+     */
     private ?int $pricePoint;
+    /**
+     * @var int|null
+     */
     private ?int $priceEuro;
+    /**
+     * @var string|null
+     */
     private ?string $pathImg;
 
     /**
@@ -50,97 +84,154 @@ class Article {
     }
 
 
-
-
-
-
-
-
+    /**
+     * @return int
+     */
     public function getId(): int
     {
         return $this->id;
     }
 
+    /**
+     * @param int|null $id
+     * @return void
+     */
     public function setId(?int $id): void
     {
         $this->id = $id;
     }
 
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * @param string|null $name
+     * @return void
+     */
     public function setName(?string $name): void
     {
         $this->name = $name;
     }
 
+    /**
+     * @return Type
+     */
     public function getType(): Type
     {
         return $this->type;
     }
 
+    /**
+     * @param Type|null $type
+     * @return void
+     */
     public function setType(?Type $type): void
     {
         $this->type = $type;
     }
 
+    /**
+     * @return string|null
+     */
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
+    /**
+     * @param string|null $description
+     * @return void
+     */
     public function setDescription(?string $description): void
     {
         $this->description = $description;
     }
 
+    /**
+     * @return DateTime
+     */
     public function getCreatedAt(): DateTime
     {
         return $this->createdAt;
     }
 
+    /**
+     * @param DateTime|null $createdAt
+     * @return void
+     */
     public function setCreatedAt(?DateTime $createdAt): void
     {
         $this->createdAt = $createdAt;
     }
 
+    /**
+     * @return DateTime|null
+     */
     public function getUpdatedAt(): ?DateTime
     {
         return $this->updatedAt;
     }
 
+    /**
+     * @param DateTime|null $updatedAt
+     * @return void
+     */
     public function setUpdatedAt(?DateTime $updatedAt): void
     {
         $this->updatedAt = $updatedAt;
     }
 
+    /**
+     * @return int|null
+     */
     public function getPricePoint(): ?int
     {
         return $this->pricePoint;
     }
 
+    /**
+     * @param int|null $pricePoint
+     * @return void
+     */
     public function setPricePoint(?int $pricePoint): void
     {
         $this->pricePoint = $pricePoint;
     }
 
+    /**
+     * @return int|null
+     */
     public function getPriceEuro(): ?int
     {
         return $this->priceEuro;
     }
 
+    /**
+     * @param int|null $priceEuro
+     * @return void
+     */
     public function setPriceEuro(?int $priceEuro): void
     {
         $this->priceEuro = $priceEuro;
     }
 
+    /**
+     * @return string|null
+     */
     public function getPathImg(): ?string
     {
         return $this->pathImg;
     }
 
+    /**
+     * @param string|null $pathImg
+     * @return void
+     */
     public function setPathImg(?string $pathImg): void
     {
         $this->pathImg = $pathImg;
