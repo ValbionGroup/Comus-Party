575aa061d05160c43f6833a4bce1766707f79378
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-11-12 17:30:43 +0100

    :memo: Modification de la documentation du fichier `controllerFactory.class.php`

--- src/controllers/controllerFactory.class.php
+++ src/controllers/controllerFactory.class.php
@@ -1,7 +1,30 @@
 <?php
+/**
+ * @file    controllerFactory.class.php
+ * @author  Estéban DESESSARD
+ * @brief   Le fichier contient la déclaration & définition de la classe ControllerFactory.
+ * @date    12/11/2024
+ * @version 0.1
+ */
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+/**
+ * @brief   La classe ControllerFactory est un patron de conception Factory
+ * @remark  C'est une version simplifié du patron de conception Factory
+ */
 class ControllerFactory {
-    public static function getController($controller, \Twig\Loader\FilesystemLoader $loader, \Twig\Environment $twig) {
+    /**
+     * @brief  La méthode getController permet de récupérer un contrôleur
+     * @param $controller
+     * @param FilesystemLoader $loader
+     * @param Environment $twig
+     * @return mixed
+     * @throws Exception
+     */
+    public static function getController($controller, FilesystemLoader $loader, Environment $twig)
+    {
         $controllerName = "Controller" . ucfirst($controller);
         if (!class_exists($controllerName)) {
             throw new Exception('Controller ' . $controllerName . ' not found');
