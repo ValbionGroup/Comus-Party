5c549883fb1033cd2134d3d2445b6ef648d87cbf
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-11-12 17:38:57 +0100

    :memo: Modification de la documentation du fichier `controllerPlayer.class.php`

--- src/controllers/controllerPlayer.class.php
+++ src/controllers/controllerPlayer.class.php
@@ -1,10 +1,40 @@
 <?php
+/**
+ * @file    controllerPlayer.class.php
+ * @author  Estéban DESESSARD
+ * @brief   Le fichier contient la déclaration & définition de la classe ControllerPlayer.
+ * @date    12/11/2024
+ * @version 0.1
+ */
 
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Loader\FilesystemLoader;
+
+/**
+ * @brief   La classe ControllerPlayer permet de gérer les actions liées à un joueur
+ */
 class ControllerPlayer extends Controller {
-    public function __construct(\Twig\Loader\FilesystemLoader $loader, \Twig\Environment $twig) {
+    /**
+     * @brief                           Constructeur de la classe ControllerPlayer
+     * @param FilesystemLoader $loader Le loader de Twig
+     * @param Environment $twig L'environnement de Twig
+     */
+    public function __construct(FilesystemLoader $loader, Environment $twig)
+    {
         parent::__construct($loader, $twig);
     }
 
+    /**
+     * @brief                               La méthode afficher permet d'afficher le profil d'un joueur
+     * @return void                         Objet retourné par la méthode, ici rien
+     * @throws DateMalformedStringException Exception levée dans le cas d'une date malformée
+     * @throws LoaderError                  Exception levée dans le cas d'une erreur de chargement
+     * @throws RuntimeError                 Exception levée dans le cas d'une erreur d'exécution
+     * @throws SyntaxError                  Exception levée dans le cas d'une erreur de syntaxe
+     */
     public function afficher() {
         if (!isset($_GET['uuid'])) {
             header('Location: ../pages/404.php');
