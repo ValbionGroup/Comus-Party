856748af45af45d5e2237ae19c997c75506d96fb
Lucas E. <espiet.l@valbion.com>
2024-11-21 17:21:54 +0100

    :pencil2: Correction de l'exception

--- src/Models/Exceptions/MalformedRequestException.php
+++ src/Models/Exceptions/MalformedRequestException.php
@@ -8,5 +8,13 @@ use Throwable;
 
 class MalformedRequestException extends Exception
 {
+    public function __construct(string $message = "", int $code = 400, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 
+    public function __toString()
+    {
+        return __CLASS__ . ": [$this->code]: $this->message\n";
+    }
 }
