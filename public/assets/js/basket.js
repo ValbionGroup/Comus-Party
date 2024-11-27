1f1f67b598c9ec57ea59be08d195aae37852027c
TistouCode <soldarmetv@gmail.com>
2024-11-27 14:10:15 +0100

    :lipstick: Ajout d'une notification lorsqu'un article est retiré du panier

--- public/assets/js/basket.js
+++ public/assets/js/basket.js
@@ -40,6 +40,17 @@ removeButtons.forEach(button => {
         const parentDiv = this.parentElement.parentElement;
         parentDiv.remove();
         testArticleDansPanier()
+        notificationMessage.textContent = "Article retiré du panier"
+        notification.className = "z-50 fixed bottom-5 right-5 bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg opacity-0 transform scale-90 transition-all duration-300 ease-in-out";
+        // Afficher la notification
+        notification.classList.remove('opacity-0', 'scale-90');
+        notification.classList.add('opacity-100', 'scale-100');
+
+        // Masquer la notification après 5 secondes
+        setTimeout(() => {
+            notification.classList.remove('opacity-100', 'scale-100');
+            notification.classList.add('opacity-0', 'scale-90');
+        }, 5000);
 
     });
 });
