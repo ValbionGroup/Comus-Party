d6e58912dd63e0f408bfd33a6dd8376bdfded295
TistouCode <soldarmetv@gmail.com>
2024-11-28 10:18:42 +0100

    :ambulance: Ajout de l'affichage de la page shop dans le routeur

--- src/routes.php
+++ src/routes.php
@@ -17,10 +17,10 @@ $router->get('/game/:code', function ($code) {
     exit;
 });
 
-$router->get('/shop', function () {
-    echo "Page de la boutique<br/>";
-    echo "A IMPLEMENTER";
+$router->get('/shop', function () use ($loader, $twig){
+    ControllerFactory::getController("shop",$loader,$twig)->call("show");
     exit;
+
 });
 
 $router->get('/shop/basket', function () {
