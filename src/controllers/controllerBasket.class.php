58cf040e7fde949299708d39d94c1f37db9fddf8
TistouCode <soldarmetv@gmail.com>
2024-11-28 09:00:43 +0100

    :memo: Ajout de documentation

--- src/controllers/controllerBasket.class.php
+++ src/controllers/controllerBasket.class.php
@@ -64,7 +64,7 @@ class ControllerBasket extends Controller
         if ($id != null) {
             $id_article = intval($id);
 
-            // Ajouter l'ID de l'article au panier s'il n'y est pas déjà
+            // Retirer l'ID de l'article au panier s'il n'y est pas déjà
             if (in_array($id_article, $_SESSION['basket'])) {
                 $key = array_search($id_article, $_SESSION['basket'], true);
                 if ($key !== false) {
