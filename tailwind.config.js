b19168ceb9e37a5e2ad5686085b82e373f4f7a31
TistouCode <soldarmetv@gmail.com>
2024-11-29 14:09:38 +0100

    :ambulance: Correction de la compilation du tailwind dans le dossier public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,html,php,twig}", "./public/**/*.{js,html,php,twig,css}"],
+  content: ["./src/**/*.{js,html,php,twig}"],
   theme: {
     fontFamily: {
       'serif': ["'Londrina Solid'", "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
