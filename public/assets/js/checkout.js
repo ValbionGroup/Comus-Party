49daae430b6038b871cbb07f64a182fe0cc72cb1
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2024-11-29 15:53:48 +0100

    :sparkles: Création de **checkout.js** pour gérer le formulaire de paiement

---
+++ public/assets/js/checkout.js
@@ -0,0 +1,8 @@
+/**
+ *   @file checkout.js
+ *   @author Conchez-Boueytou Robin
+ *   @brief Fichier js permettant de gérer le formulaire de paiement
+ *   @details
+ *   @date 29/11/2024
+ *   @version 0.1
+ */
