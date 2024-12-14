ff1c113b08bd219bc35655b6a5759515557d7394
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-14 11:51:40 +0100

    :memo: Documentation de la classe `ControllerPolicy`

--- src/Controllers/controllerPolicy.class.php
+++ src/Controllers/controllerPolicy.class.php
@@ -1,17 +1,43 @@
 <?php
+/**
+ * @file    controllerPolicy.class.php
+ * @brief   Ce fichier contient la déclaration & définition de la classe ControllerPolicy.
+ * @author  Estéban DESESSARD
+ * @date    14/12/2024
+ * @version 1.0
+ */
 
 namespace ComusParty\Controllers;
 
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 use Twig\Loader\FilesystemLoader;
 
+/**
+ * @brief Classe ControllerPolicy
+ * @details Contrôleur des différentes pages de politiques du site (CGU, CGV, etc.)
+ */
 class ControllerPolicy extends Controller
 {
+    /**
+     * @brief Constructeur de la classe ControllerPolicy
+     * @param FilesystemLoader $loader Le loader de Twig
+     * @param Environment $twig L'environnement de Twig
+     */
     public function __construct(FilesystemLoader $loader, Environment $twig)
     {
         parent::__construct($loader, $twig);
     }
 
+    /**
+     * @brief Affiche la page des Conditions Générales d'Utilisation
+     * @return void
+     * @throws LoaderError Exception levée dans le cas d'une erreur de chargement
+     * @throws RuntimeError Exception levée dans le cas d'une erreur d'exécution
+     * @throws SyntaxError Exception levée dans le cas d'une erreur de syntaxe
+     */
     public function showCgu()
     {
         $template = $this->getTwig()->load('cgu.twig');
