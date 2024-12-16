bfa73a21af8f17747057d64dbd13ab5274aab786
Lucas ESPIET <lespiet@iutbayonne.univ-pau.fr>
2024-12-16 12:28:27 +0100

    :sparkles: Ajout d'une exception lorsqu'un jeu est indisponible

---
+++ src/Models/Exceptions/GameUnavailableException.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @brief Gère les exceptions liées à l'indisponibilité d'un jeu
+ *
+ * @file GameUnavailableException.php
+ * @author Lucas ESPIET "espiet.l@valbion.com"
+ * @version 1.0
+ * @date 2024-12-16
+ */
+
+namespace ComusParty\Models\Exceptions;
+
+use Exception;
+
+/**
+ * @brief Classe GameUnavailableException
+ * @details Exception personnalisée levée lorsqu'un jeu est indisponible
+ */
+class GameUnavailableException extends Exception
+{
+    public function __construct(string $message = "", int $code = 503, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [$this->code]: $this->message\n";
+    }
+}
