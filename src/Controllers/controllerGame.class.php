a253a3bc7558bdd4eae8a124731d4445d0181a5f
Lucas ESPIET <lespiet@iutbayonne.univ-pau.fr>
2024-12-16 17:15:35 +0100

    :sparkles: Ajout des fonction de récupération des jeux

--- src/Controllers/controllerGame.class.php
+++ src/Controllers/controllerGame.class.php
@@ -9,7 +9,9 @@
 
 namespace ComusParty\Controllers;
 
+use ComusParty\Models\Exceptions\GameUnavailableException;
 use ComusParty\Models\GameDao;
+use ComusParty\Models\GameState;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -47,5 +49,56 @@ class ControllerGame extends Controller
         echo $template->render(array(
             "games" => $games
         ));
+    }
+
+    /**
+     * @brief Permet d'initialiser le jeu après validation des paramètres et que les joueurs soient prêts
+     *
+     * @param int $id ID du jeu
+     * @param array $settings Paramètres du jeu
+     * @param array $players Joueurs de la partie
+     * @throws GameUnavailableException
+     */
+    public function initGame(int $id, array $settings, array $players): void
+    {
+        $game = (new GameDao($this->getPdo()))->findById($id);
+
+        if ($game->getState() != GameState::AVAILABLE) {
+            throw new GameUnavailableException("Le jeu n'est pas disponible");
+        }
+
+        $gameFolder = $this->getGameFolder($id);
+        $settings = $this->getGameSettings($id);
+
+        if (sizeof($settings) == 0) {
+        }
+    }
+
+    /**
+     * @brief Récupère le dossier du jeu dont l'UUID est passé en paramètre
+     *
+     * @param int $id
+     * @return string Chemin du dossier du jeu
+     */
+    private function getGameFolder(int $id): string
+    {
+        return realpath(__DIR__ . "/../..") . "/games/$id";
+    }
+
+    /**
+     * @brief Récupère les paramètres du jeu dont l'UUID est passé en paramètre
+     *
+     * @param int $id ID du jeu
+     * @return array Tableau associatif contenant les paramètres du jeu
+     */
+    private function getGameSettings(int $id): array
+    {
+        $gameFolder = $this->getGameFolder($id);
+        $settingsFile = "$gameFolder/settings.json";
+        if (!file_exists($settingsFile)) {
+            return [];
+        }
+
+        return json_decode(file_get_contents($settingsFile), true);
     }
 }
