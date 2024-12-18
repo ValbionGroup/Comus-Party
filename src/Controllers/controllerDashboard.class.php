956fa3fe372cc33d63560a99c24c126fc51ec573
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-18 10:56:12 +0100

    :sparkles: Création de la classe `ControllerDashboard`

---
+++ src/Controllers/controllerDashboard.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file controllerDashboard.class.php
+ * @brief Le fichier contient la déclaration et la définition de la classe ControllerDashboard
+ * @author Estéban DESESSARD
+ * @date 18/12/2024
+ * @version 1.0
+ */
+
+namespace ComusParty\Controllers;
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Loader\FilesystemLoader;
+
+class ControllerDashboard extends Controller
+{
+    /**
+     * @brief Constructeur de la classe ControllerDashboard
+     * @param FilesystemLoader $loader
+     * @param Environment $twig
+     */
+    public function __construct(FilesystemLoader $loader, Environment $twig)
+    {
+        parent::__construct($loader, $twig);
+    }
+
+    /**
+     * @brief Affiche la page d'accueil du dashboard
+     * @return void
+     * @throws LoaderError Exception levée dans le cas d'une erreur de chargement du template
+     * @throws RuntimeError Exception levée dans le cas d'une erreur d'exécution
+     * @throws SyntaxError Exception levée dans le cas d'une erreur de syntaxe
+     */
+    public function showDashboard()
+    {
+        $template = $this->getTwig()->load('moderator/dashboard.twig');
+        echo $template->render();
+    }
+}
