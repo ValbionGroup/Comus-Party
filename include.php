ef9156e76d45e9fd819badd66a5ec9f135f41168
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2024-12-18 09:27:09 +0100

    :sparkles: Ajout des classes Moderator et ModeratorDao dans le fichier d'inclusion

--- include.php
+++ include.php
@@ -49,6 +49,8 @@ require_once __DIR__ . '/src/Models/passwordResetToken.class.php';
 require_once __DIR__ . '/src/Models/passwordResetToken.dao.php';
 require_once __DIR__ . '/src/Models/suggestion.class.php';
 require_once __DIR__ . '/src/Models/suggestion.dao.php';
+require_once __DIR__ . '/src/Models/moderator.class.php';
+require_once __DIR__ . '/src/Models/moderator.dao.php';
 
 session_start();
 require_once __DIR__ . '/config/twig.php';
