9d75c93f84d7e7d3a47ec269f746e80f55501902
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-18 15:44:34 +0100

    :sparkles: Complétion de la classe `Suggestion` avec de nouveaux attributs

--- src/Models/suggestion.class.php
+++ src/Models/suggestion.class.php
@@ -9,6 +9,8 @@
 
 namespace ComusParty\Models;
 
+use DateTime;
+
 /**
  * @brief Classe Suggestion
  * @details La classe Suggestion répertorie une suggestion effectuée par un utilisateur de la plateforme
@@ -28,17 +30,44 @@ class Suggestion
     private ?string $authorUuid;
 
     /**
+     * @brief Le nom d'utilisateur de l'auteur de la suggestion
+     * @var string|null
+     */
+    private ?string $authorUsername;
+
+    /**
+     * @brief La date de création de la suggestion
+     * @var DateTime|null
+     */
+    private ?DateTime $createdAt;
+
+    /**
+     * @brief L'UUID du modérateur ayant traité la suggestion
+     * @var string|null
+     */
+    private ?string $treatedBy;
+
+    /**
      * @brief Le constructeur de la classe Suggestion
-     * @param string|null $content
-     * @param string|null $authorUuid
+     * @param string|null $content Le contenu de la suggestion
+     * @param string|null $authorUuid L'UUID de l'auteur de la suggestion
+     * @param string|null $authorUsername Le nom d'utilisateur de l'auteur de la suggestion
+     * @param DateTime|null $createdAt La date de création de la suggestion
+     * @param string|null $treatedBy L'UUID du modérateur ayant traité la suggestion
      */
     public function __construct(
-        ?string $content = null,
-        ?string $authorUuid = null
+        ?string   $content = null,
+        ?string   $authorUuid = null,
+        ?string   $authorUsername = null,
+        ?DateTime $createdAt = null,
+        ?string   $treatedBy = null
     )
     {
         $this->content = $content;
         $this->authorUuid = $authorUuid;
+        $this->authorUsername = $authorUsername;
+        $this->createdAt = $createdAt;
+        $this->treatedBy = $treatedBy;
     }
 
     /**
@@ -77,5 +106,62 @@ class Suggestion
     public function setAuthorUuid(?string $authorUuid): void
     {
         $this->authorUuid = $authorUuid;
+    }
+
+    /**
+     * @brief Permet de récupérer le nom d'utilisateur de l'auteur de la suggestion
+     * @return string|null Le nom d'utilisateur de l'auteur de la suggestion
+     */
+    public function getAuthorUsername(): ?string
+    {
+        return $this->authorUsername;
+    }
+
+    /**
+     * @brief Permet de définir le nom d'utilisateur de l'auteur de la suggestion
+     * @param string|null $authorUsername Le nom d'utilisateur de l'auteur de la suggestion
+     * @return void
+     */
+    public function setAuthorUsername(?string $authorUsername): void
+    {
+        $this->authorUsername = $authorUsername;
+    }
+
+    /**
+     * @brief Permet de récupérer la date de création de la suggestion
+     * @return DateTime|null La date de création de la suggestion
+     */
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @brief Permet de définir la date de création de la suggestion
+     * @param DateTime|null $createdAt La date de création de la suggestion
+     * @return void
+     */
+    public function setCreatedAt(?DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @brief Permet de récupérer l'UUID du modérateur ayant traité la suggestion
+     * @return string|null L'UUID du modérateur ayant traité la suggestion
+     */
+    public function getTreatedBy(): ?string
+    {
+        return $this->treatedBy;
+    }
+
+    /**
+     * @brief Permet modifier l'UUID du modérateur ayant traité la suggestion
+     * @param string|null $treatedBy L'UUID du modérateur ayant traité la suggestion
+     * @return void
+     */
+    public function setTreatedBy(?string $treatedBy): void
+    {
+        $this->treatedBy = $treatedBy;
     }
 }
