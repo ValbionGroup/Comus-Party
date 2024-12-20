6b7c70dd47c686154e821763b9f7aa0125b2e882
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-20 10:33:42 +0100

    :fire: Suppression d'un `console.log()`

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -81,7 +81,6 @@ function denySuggest(e) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             let response = JSON.parse(xhr.responseText);
-            console.log(response);
             if (response.success) {
                 closeModal();
                 location.reload();
