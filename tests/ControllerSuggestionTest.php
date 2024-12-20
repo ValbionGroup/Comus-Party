82351363a50db721352291e2fff4128fefb48803
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-20 11:15:54 +0100

    :memo: Documentation des tests

--- tests/ControllerSuggestionTest.php
+++ tests/ControllerSuggestionTest.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @file ControllerSuggestion.php
+ * @brief Le fichier contient la déclaration et la définition de la classe Suggestion
+ * @author Estéban DESESSARD
+ * @date 20/12/2024
+ * @version 0.1
+ */
 
 require_once __DIR__ . '/../include.php';
 
@@ -8,10 +15,22 @@ use PHPUnit\Framework\TestCase;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
+/**
+ * @brief Classe ControllerSuggestionTest
+ * @details La classe ControllerSuggestionTest permet de tester les méthodes de la classe ControllerSuggestion
+ */
 class ControllerSuggestionTest extends TestCase
 {
+    /**7
+     * @brief Controller
+     * @var Controller|ControllerSuggestion
+     */
     private Controller $controller;
 
+    /**
+     * @brief Test de la méthode sendSuggestion avec des données valides
+     * @return void
+     */
     public function testSendSuggestionWithValidDatas(): void
     {
         // Démarrer la session si ce n'est pas déjà fait
@@ -30,6 +49,10 @@ class ControllerSuggestionTest extends TestCase
         session_destroy();
     }
 
+    /**
+     * @brief Test de la méthode sendSuggestion avec un objet invalide
+     * @return void
+     */
     public function testSendSuggestionWithInvalidObjectThrowUnhandledMatchError(): void
     {
         // Démarrer la session si ce n'est pas déjà fait
@@ -48,6 +71,10 @@ class ControllerSuggestionTest extends TestCase
         session_destroy();
     }
 
+    /**
+     * @brief Test de la méthode sendSuggestion avec un UUID inexistant en base de données
+     * @return void
+     */
     public function testSendSuggestionWithUnexistantUuidThrowPDOException(): void
     {
         // Démarrer la session si ce n'est pas déjà fait
@@ -66,6 +93,11 @@ class ControllerSuggestionTest extends TestCase
         session_destroy();
     }
 
+    /**
+     * @brief Test de la méthode sendSuggestion avec un username inexistant en base de données
+     * @details Le nom d'utilisateur ne pose aucun problème car il ne constitue aucune clé étrangère en base de données qui pourrait empêcher l'insertion de la suggestion
+     * @return void
+     */
     public function testSendSuggestionWithUnexistantUsernameDontThrowPDOException(): void
     {
         // Démarrer la session si ce n'est pas déjà fait
@@ -84,6 +116,10 @@ class ControllerSuggestionTest extends TestCase
         session_destroy();
     }
 
+    /**
+     * @brief Test de la méthode sendSuggestion avec un UUID null
+     * @return void
+     */
     public function testSendSuggestionWithNullUuidThrowPDOException(): void
     {
         // Démarrer la session si ce n'est pas déjà fait
