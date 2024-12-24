417d91bf0ab68b6413247dd525607498a4a7c05f
Lucas E. <espiet.l@valbion.com>
2024-12-24 19:05:31 +0100

    :memo: Amélioration de la documentation pour `ControllerGame`

--- src/Controllers/controllerGame.class.php
+++ src/Controllers/controllerGame.class.php
@@ -19,6 +19,7 @@ use ComusParty\Models\GameRecordState;
 use ComusParty\Models\GameState;
 use ComusParty\Models\PlayerDAO;
 use DateTime;
+use Exception;
 use Random\RandomException;
 use Twig\Environment;
 use Twig\Error\LoaderError;
@@ -114,17 +115,6 @@ class ControllerGame extends Controller
     }
 
     /**
-     * @brief Récupère les paramètres modifiables du jeu dont l'ID est passé en paramètre
-     * @param int $id ID du jeu
-     * @return array Tableau associatif contenant les paramètres modifiables du jeu
-     */
-    private function getGameModiableSettings(int $id): array
-    {
-        $allSettings = $this->getGameSettings($id);
-        return $allSettings["modifiableSettings"];
-    }
-
-    /**
      * @brief Récupère les paramètres du jeu dont l'ID est passé en paramètre
      *
      * @param int $id ID du jeu
@@ -146,7 +136,8 @@ class ControllerGame extends Controller
      * @param int|null $id L'identifiant du jeu
      * @return void
      */
-    public function getGameInformations(?int $id) {
+    public function getGameInformations(?int $id)
+    {
         $gameManager = new GameDAO($this->getPdo());
         $game = $gameManager->findWithDetailsById($id);
         echo json_encode([
@@ -161,6 +152,16 @@ class ControllerGame extends Controller
         exit;
     }
 
+    /**
+     * @brief Affiche la page de la partie dont le code est passé en paramètre
+     * @param string $code Code de la partie
+     * @return void
+     * @throws NotFoundException Exception levée si la partie n'existe pas
+     * @throws SyntaxError Exception levée dans le cas d'une erreur de syntaxe
+     * @throws RuntimeError Exception levée dans le cas d'une erreur d'exécution
+     * @throws LoaderError Exception levée dans le cas d'une erreur de chargement du template
+     * @throws Exception Exception levée en cas d'erreur avec la base de données
+     */
     public function showGame(string $code): void
     {
         $gameRecord = (new GameRecordDAO($this->getPdo()))->findByUuid($code);
@@ -208,11 +209,23 @@ class ControllerGame extends Controller
     }
 
     /**
+     * @brief Récupère les paramètres modifiables du jeu dont l'ID est passé en paramètre
+     * @param int $id ID du jeu
+     * @return array Tableau associatif contenant les paramètres modifiables du jeu
+     */
+    private function getGameModiableSettings(int $id): array
+    {
+        $allSettings = $this->getGameSettings($id);
+        return $allSettings["modifiableSettings"];
+    }
+
+    /**
      * @brief Quitte une partie
      * @param string $code UUID de la partie à quitter
      * @param string $playerUuid UUID du joueur qui quitte la partie
      * @return void
      * @throws NotFoundException Exception levée si la partie n'existe pas
+     * @throws Exception Exception levée en cas d'erreur avec la base de données
      */
     public function quitGame(string $code, string $playerUuid): void
     {
@@ -241,6 +254,7 @@ class ControllerGame extends Controller
      * @return void
      * @throws GameUnavailableException Exception levée si le jeu n'est pas disponible
      * @throws RandomException Exception levée en cas d'erreur lors de la génération du code
+     * @throws Exception Exception levée en cas d'erreur avec la base de données
      */
     public function createGame(int $gameId): void
     {
