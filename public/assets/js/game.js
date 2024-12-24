8aa00c83d45932c2205ef7682525271d95b8a92c
Lucas E. <espiet.l@valbion.com>
2024-12-24 18:58:47 +0100

    :sparkles: Ajout des fonctions d'envoi et de réception de messages chat

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -8,3 +8,36 @@ function quitGameAndBackHome(gameCode) {
         throw new Error('Failed to quit game');
     });
 }
+
+function sendChatMessage() {
+    const messageInput = document.getElementById('chatInput');
+    const messages = document.getElementById('chatContent');
+    const messageItem = document.createElement('p');
+    messageItem.textContent = messageInput.value;
+    messages.appendChild(messageItem);
+    messageInput.value = '';
+    conn.send(messageItem.textContent);
+}
+
+function receiveChatMessage(message) {
+    const messages = document.getElementById('chatContent');
+    const messageItem = document.createElement('p');
+    messageItem.textContent = message;
+    messages.appendChild(messageItem);
+}
+
+// WebSocket
+
+const conn = new WebSocket('ws://localhost:8315/chat/945290a1a2e2d723da48640e73c5d76d');
+conn.onopen = function (e) {
+    console.log("Connexion établie !");
+};
+conn.onmessage = function (e) {
+    receiveChatMessage(e.data);
+};
+document.getElementById('sendChat').onclick = sendChatMessage;
+document.getElementById('chatInput').addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+        sendChatMessage();
+    }
+});
