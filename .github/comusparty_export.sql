0b4cd4a06fe83fc9b890a4a425811c888c36c61f
ESPIET L. <espiet.l@valbion.com>
2025-01-03 17:42:46 +0100

    :pencil2: Correctif dans les requêtes d'insertions

--- .github/comusparty_export.sql
+++ .github/comusparty_export.sql
@@ -138,11 +138,11 @@ CREATE TABLE `cp_game_record`
 -- Déchargement des données de la table `cp_game_record`
 --
 
-INSERT INTO `cp_game_record` (`uuid`, `game_id`, `hosted_by`, `state`, `created_at`, `updated_at`)
-VALUES ('game_rec_uuid1', 1, 'uuid1', 'started', '2024-11-13 15:18:39', '2024-11-13 15:18:39'),
-       ('game_rec_uuid2', 2, 'uuid2', 'waiting', '2024-11-13 15:18:39', '2024-11-13 15:18:39'),
-       ('game_rec_uuid3', 3, 'uuid3', '', '2024-11-14 08:15:01', '2024-11-14 08:15:01'),
-       ('game_rec_uuid4', 4, 'uuid4', 'waiting', '2024-11-14 08:15:01', '2024-11-14 08:15:01');
+INSERT INTO `cp_game_record` (`uuid`, `game_id`, `hosted_by`, `state`, `private`, `created_at`, `updated_at`)
+VALUES ('game_rec_uuid1', 1, 'uuid1', 'started', 0, '2024-11-13 15:18:39', '2024-11-13 15:18:39'),
+       ('game_rec_uuid2', 2, 'uuid2', 'waiting', 1, '2024-11-13 15:18:39', '2024-11-13 15:18:39'),
+       ('game_rec_uuid3', 3, 'uuid3', '', 1, '2024-11-14 08:15:01', '2024-11-14 08:15:01'),
+       ('game_rec_uuid4', 4, 'uuid4', 'waiting', 0, '2024-11-14 08:15:01', '2024-11-14 08:15:01');
 
 -- --------------------------------------------------------
 
