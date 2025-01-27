e4bc1f7e05560d29e57656fbde928736d90b4e96
TistouCode <soldarmetv@gmail.com>
2025-01-27 10:03:51 +0100

    :zap: Ajout d'un try catch lors de l'envoie de l'email
    Signed-off-by: TistouCode <soldarmetv@gmail.com>

--- src/Controllers/controllerAuth.class.php
+++ src/Controllers/controllerAuth.class.php
@@ -473,16 +473,25 @@ class ControllerAuth extends Controller
                 'res' => $res
             ]);
         }else{
-            $email = $userDAO->findEmailByUuid($_SESSION['uuid']);
-            $subject = 'Modification de mot-de-passe';
-            $message = '<p>Vous venez de modifier votre mot de passe sur Comus Party !</p>';
-            $confirmMail = new Mailer(array($email), $subject, $message);
-            $confirmMail->generateHTMLMessage();
-            $confirmMail->send();
-            echo json_encode([
-                'success' => true,
-                'res' => $res
-            ]);
+            try{
+                $email = $userDAO->findEmailByUuid($_SESSION['uuid']);
+                $subject = 'Modification de mot-de-passe';
+                $message = '<p>Vous venez de modifier votre mot de passe sur Comus Party !</p>';
+                $confirmMail = new Mailer(array($email), $subject, $message);
+                $confirmMail->generateHTMLMessage();
+                $confirmMail->send();
+                echo json_encode([
+                    'success' => true,
+                    'res' => $res
+                ]);
+            }catch (Exception $e){
+                echo json_encode([
+                    'success' => false,
+                    'res' => $res,
+                    'error' => $e->getMessage()
+                ]);
+            }
+
         }
     }
 
