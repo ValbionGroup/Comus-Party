414b54ba6f9b25a7bce36242fb1c5576eaad6319
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2025-01-30 11:57:39 +0100

    :sparkles: Déclaration unique de chacun des éléments HTML

--- public/assets/js/authentication.js
+++ public/assets/js/authentication.js
@@ -3,8 +3,23 @@
  * @brief   Le fichier contient les fonctions de vérification des champs de formulaire
  *          de l'authentification.
  * @date    14/11/2024
- * @version 1.0
+ * @version 2.0
  */
+
+const INPUT_USERNAME = document.getElementById("username");
+const INPUT_EMAIL = document.getElementById("email");
+const INPUT_PASSWORD = document.getElementById("password");
+const INPUT_CONFIRM_PASSWORD = document.getElementById("passwordConfirm");
+const INPUT_TERMS_OF_SERVICE = document.getElementById("termsOfService");
+const INPUT_PRIVACY_POLICY = document.getElementById("privacyPolicy");
+const TOGGLE_PASSWORD_BUTTON = document.getElementById('togglePassword');
+const EYE_OPEN_ICON = document.getElementById('eyeOpenIcon');
+const EYE_CLOSED_ICON = document.getElementById('eyeClosedIcon');
+const TOGGLE_PASSWORD_CONFIRM_BUTTON = document.getElementById('togglePasswordConfirm');
+const EYE_OPEN_ICON_CONFIRM = document.getElementById('eyeOpenIconConfirm');
+const EYE_CLOSED_ICON_CONFIRM = document.getElementById('eyeClosedIconConfirm');
+const SUBMIT_BUTTON = document.getElementById("submitButton");
+
 
 /**
  * @brief Fonction executée lorsque la page est chargée.
@@ -15,24 +30,12 @@
  * @return void
  */
 window.onload = function() {
-    // Récupère les inputs du formulaire
     // Ajoute un listener sur l'event "input" des inputs
-    const INPUT_USERNAME = document.getElementById("username");
     INPUT_USERNAME.addEventListener("input", checkConditions);
-
-    const INPUT_EMAIL = document.getElementById("email");
     INPUT_EMAIL.addEventListener("input", checkConditions);
-
-    const INPUT_PASSWORD = document.getElementById("password");
     INPUT_PASSWORD.addEventListener("input", checkConditions);
-
-    const INPUT_CONFIRM_PASSWORD = document.getElementById("passwordConfirm");
     INPUT_CONFIRM_PASSWORD.addEventListener("input", checkConditions);
-
-    const INPUT_TERMS_OF_SERVICE = document.getElementById("termsOfService");
     INPUT_TERMS_OF_SERVICE.addEventListener("change", checkConditions);
-
-    const INPUT_PRIVACY_POLICY = document.getElementById("privacyPolicy");
     INPUT_PRIVACY_POLICY.addEventListener("change", checkConditions);
 
     addPasswordVisibilityListeners();
@@ -49,16 +52,6 @@ window.onload = function() {
  */
 function addPasswordVisibilityListeners()
 {
-    const INPUT_PASSWORD = document.getElementById("password");
-    const TOGGLE_PASSWORD_BUTTON = document.getElementById('togglePassword');
-    const EYE_OPEN_ICON = document.getElementById('eyeOpenIcon');
-    const EYE_CLOSED_ICON = document.getElementById('eyeClosedIcon');
-
-    const INPUT_CONFIRM_PASSWORD = document.getElementById("passwordConfirm");
-    const TOGGLE_PASSWORD_CONFIRM_BUTTON = document.getElementById('togglePasswordConfirm');
-    const EYE_OPEN_ICON_CONFIRM = document.getElementById('eyeOpenIconConfirm');
-    const EYE_CLOSED_ICON_CONFIRM = document.getElementById('eyeClosedIconConfirm');
-
     TOGGLE_PASSWORD_BUTTON.addEventListener('click', () => {
         // Toggle visibilité password
         const isPasswordHidden = INPUT_PASSWORD.type === 'password';
@@ -91,15 +84,6 @@ function addPasswordVisibilityListeners()
  * @return void
  */
 function checkConditions(event) {
-    // Variables
-    let submitButton = document.getElementById("submitButton");
-    let inputUsername = document.getElementById("username");
-    let inputEmail = document.getElementById("email");
-    let inputPassword = document.getElementById("password");
-    let inputConfirmPassword = document.getElementById("passwordConfirm");
-    let inputTermsOfService = document.getElementById("termsOfService");
-    let inputPrivacyPolicy = document.getElementById("privacyPolicy");
-
     // Constantes
     const MIN_USERNAME_LENGTH = 3;
     const MAX_USERNAME_LENGTH = 120;
@@ -113,23 +97,23 @@ function checkConditions(event) {
     const ALLOWED_USERNAME_CHARACTERS = /^[a-zA-Z0-9_-]+$/;
 
     // Vérifications
-    const isUsernameValid = inputUsername.value.length >= MIN_USERNAME_LENGTH && ALLOWED_USERNAME_CHARACTERS.test(inputUsername.value) && inputUsername.value.length <= MAX_USERNAME_LENGTH;
-    const isEmailValid = EMAIL_REGEX.test(inputEmail.value);
-    const isPasswordValid = inputPassword.value.length >= MIN_PASSWORD_LENGTH &&
-                            inputPassword.value.length <= MAX_PASSWORD_LENGTH &&
-                            UPPERCASE_LETTER.test(inputPassword.value) &&
-                            LOWERCASE_LETTER.test(inputPassword.value) &&
-                            NUMBERS.test(inputPassword.value) &&
-                            SPECIAL_CHARACTER.test(inputPassword.value);
-    const isPasswordMatch = inputPassword.value === inputConfirmPassword.value;
-    const isTermsAccepted = inputTermsOfService.checked;
-    const isPrivacyPolicyAccepted = inputPrivacyPolicy.checked;
+    const isUsernameValid = INPUT_PASSWORD.value.length >= MIN_USERNAME_LENGTH && ALLOWED_USERNAME_CHARACTERS.test(INPUT_USERNAME.value) && INPUT_USERNAME.value.length <= MAX_USERNAME_LENGTH;
+    const isEmailValid = EMAIL_REGEX.test(INPUT_EMAIL.value);
+    const isPasswordValid = INPUT_PASSWORD.value.length >= MIN_PASSWORD_LENGTH &&
+                            INPUT_PASSWORD.value.length <= MAX_PASSWORD_LENGTH &&
+                            UPPERCASE_LETTER.test(INPUT_PASSWORD.value) &&
+                            LOWERCASE_LETTER.test(INPUT_PASSWORD.value) &&
+                            NUMBERS.test(INPUT_PASSWORD.value) &&
+                            SPECIAL_CHARACTER.test(INPUT_PASSWORD.value);
+    const isPasswordMatch = INPUT_PASSWORD.value === INPUT_CONFIRM_PASSWORD.value;
+    const isTermsAccepted = INPUT_TERMS_OF_SERVICE.checked;
+    const isPrivacyPolicyAccepted = INPUT_PRIVACY_POLICY.checked;
 
     // Activer le bouton de soumission si toutes les conditions sont remplies
     if (isUsernameValid && isEmailValid && isPasswordValid && isPasswordMatch && isTermsAccepted && isPrivacyPolicyAccepted) {
-        submitButton.disabled = false;
+        SUBMIT_BUTTON.disabled = false;
     } else {
-        submitButton.disabled = true;
+        SUBMIT_BUTTON.disabled = true;
     }
 
     // Met à jour le message d'erreur et les styles des inputs uniquement pour l'input modifié
@@ -147,20 +131,20 @@ function checkConditions(event) {
         updateErrorMessage(input, "passwordNoSpecialCharacter", true, "");
         updateErrorMessage(input, "notMachingPasswords", true, "");
     } else {
-        if (input === inputUsername) {
+        if (input === INPUT_USERNAME) {
             updateErrorMessage(input, "usernameTooShort", input.value.length >= MIN_USERNAME_LENGTH, "Le nom d'utilisateur doit contenir au moins " + MIN_USERNAME_LENGTH + " caractères");
             updateErrorMessage(input, "usernameForbiddenCharacters", ALLOWED_USERNAME_CHARACTERS.test(input.value), "Le nom d'utilisateur ne doit contenir que des caractères alphanumériques, des tirets et des underscores");
             updateErrorMessage(input, "usernameTooLong", input.value.length <= MAX_USERNAME_LENGTH, "Le nom d'utilisateur doit contenir au plus " + MAX_USERNAME_LENGTH + " caractères");
-        } else if (input === inputEmail) {
+        } else if (input === INPUT_EMAIL) {
             updateErrorMessage(input, "incorrectEmailFormat", EMAIL_REGEX.test(input.value), "Format d'email incorrect");
-        } else if (input === inputPassword) {
+        } else if (input === INPUT_PASSWORD) {
             updateErrorMessage(input, "passwordTooShort", input.value.length >= MIN_PASSWORD_LENGTH, "Le mot de passe doit être au moins de " + MIN_PASSWORD_LENGTH + " caractères");
             updateErrorMessage(input, "passwordTooLong", input.value.length <= MAX_PASSWORD_LENGTH, "Le mot de passe doit être au maximum de " + MAX_PASSWORD_LENGTH + " caractères");
             updateErrorMessage(input, "passwordNoUppercase", UPPERCASE_LETTER.test(input.value), "Le mot de passe doit contenir au moins une majuscule");
             updateErrorMessage(input, "passwordNoLowercase", LOWERCASE_LETTER.test(input.value), "Le mot de passe doit contenir au moins une minuscule");
             updateErrorMessage(input, "passwordNoNumber", NUMBERS.test(input.value), "Le mot de passe doit contenir au moins un chiffre");
             updateErrorMessage(input, "passwordNoSpecialCharacter", SPECIAL_CHARACTER.test(input.value), "Le mot de passe doit contenir au moins un caractère spécial");
-        } else if (input === inputConfirmPassword) {
+        } else if (input === INPUT_CONFIRM_PASSWORD) {
             updateErrorMessage(input, "notMachingPasswords", inputPassword.value === input.value, "Les mots de passe ne correspondent pas");
         }
     }
