58be9a6d115ef520f09b294c16c8e89d0a516122
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2025-01-30 14:23:57 +0100

    :sparkles: Vérification de la correspondance des deux mots de passe de réinitialisation

--- public/assets/js/reset-password.js
+++ public/assets/js/reset-password.js
@@ -1,4 +1,9 @@
-function checkPassword() {
+const password = document.getElementById('password');
+const passwordConfirm = document.getElementById('passwordConfirm');
+const submitButton = document.getElementById('submitButton');
+const passwordMatchError = document.getElementById('passwordMatchError');
+
+function checkPasswordFormat() {
     const pictoPasswordLength = document.getElementById('pictoPasswordLength');
     const pictoPasswordCaps = document.getElementById('pictoPasswordCaps');
     const pictoPasswordLower = document.getElementById('pictoPasswordLower');
@@ -11,22 +16,7 @@ function checkPassword() {
     const passwordNumber = document.getElementById('passwordNumber');
     const passwordSpecialCharacter = document.getElementById('passwordSpecialCharacter');
 
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^#])[A-Za-z\d@$!%*?&^#]{8,}$/;
-    const password = document.getElementById('password').value;
-    const passwordConfirm = document.getElementById('passwordConfirm').value;
-    const submitButton = document.getElementById('submitButton');
-
-    if (regex.test(password) && regex.test(passwordConfirm) && password === passwordConfirm) {
-        submitButton.classList.remove("btn-disabled");
-        submitButton.classList.add("btn-primary");
-        submitButton.removeAttribute("disabled");
-    } else {
-        submitButton.classList.add("btn-disabled");
-        submitButton.classList.remove("btn-primary");
-        submitButton.setAttribute("disabled", "true");
-    }
-
-    if (password.length >= 8 && password.length <= 120) {
+    if (password.value.length >= 8 && password.value.length <= 120) {
         pictoPasswordLength.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 15.15l8.475-8.475q.3-.3.7-.3t.7.3t.3.713t-.3.712l-9.175 9.2q-.3.3-.7.3t-.7-.3L4.55 13q-.3-.3-.288-.712t.313-.713t.713-.3t.712.3z"/></svg>';
         passwordLength.classList.add("text-green-500");
         passwordLength.classList.remove("text-red-500");
@@ -36,7 +26,7 @@ function checkPassword() {
         passwordLength.classList.add("text-red-500");
     }
 
-    if (/[A-Z]/.test(password)) {
+    if (/[A-Z]/.test(password.value)) {
         pictoPasswordCaps.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 15.15l8.475-8.475q.3-.3.7-.3t.7.3t.3.713t-.3.712l-9.175 9.2q-.3.3-.7.3t-.7-.3L4.55 13q-.3-.3-.288-.712t.313-.713t.713-.3t.712.3z"/></svg>';
         passwordCaps.classList.add("text-green-500");
         passwordCaps.classList.remove("text-red-500");
@@ -46,7 +36,7 @@ function checkPassword() {
         passwordCaps.classList.add("text-red-500");
     }
 
-    if (/[a-z]/.test(password)) {
+    if (/[a-z]/.test(password.value)) {
         pictoPasswordLower.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 15.15l8.475-8.475q.3-.3.7-.3t.7.3t.3.713t-.3.712l-9.175 9.2q-.3.3-.7.3t-.7-.3L4.55 13q-.3-.3-.288-.712t.313-.713t.713-.3t.712.3z"/></svg>';
         passwordLower.classList.add("text-green-500");
         passwordLower.classList.remove("text-red-500");
@@ -56,7 +46,7 @@ function checkPassword() {
         passwordLower.classList.add("text-red-500");
     }
 
-    if (/\d/.test(password)) {
+    if (/\d/.test(password.value)) {
         pictoPasswordNumber.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 15.15l8.475-8.475q.3-.3.7-.3t.7.3t.3.713t-.3.712l-9.175 9.2q-.3.3-.7.3t-.7-.3L4.55 13q-.3-.3-.288-.712t.313-.713t.713-.3t.712.3z"/></svg>';
         passwordNumber.classList.add("text-green-500");
         passwordNumber.classList.remove("text-red-500");
@@ -66,7 +56,7 @@ function checkPassword() {
         passwordNumber.classList.add("text-red-500");
     }
 
-    if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password)) {
+    if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password.value)) {
         pictoPasswordSpecialCharacter.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 15.15l8.475-8.475q.3-.3.7-.3t.7.3t.3.713t-.3.712l-9.175 9.2q-.3.3-.7.3t-.7-.3L4.55 13q-.3-.3-.288-.712t.313-.713t.713-.3t.712.3z"/></svg>';
         passwordSpecialCharacter.classList.add("text-green-500");
         passwordSpecialCharacter.classList.remove("text-red-500");
@@ -74,5 +64,38 @@ function checkPassword() {
         pictoPasswordSpecialCharacter.innerHTML = '<svg class="p-0.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M20 20L4 4m16 0L4 20"/></svg>';
         passwordSpecialCharacter.classList.remove("text-green-500");
         passwordSpecialCharacter.classList.add("text-red-500");
+    }
+}
+
+function isPasswordValid(password) {
+    return password.length >= 8 && password.length <= 120 && // Longueur valide
+        /[A-Z]/.test(password) && // Majuscule
+        /[a-z]/.test(password) && // Minuscule
+        /\d/.test(password) && // Chiffre
+        /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password); // Caractère spécial
+}
+
+function checkPassword() {
+    const passwordsMatch = password.value === passwordConfirm.value;
+    const isValid = isPasswordValid(password.value);
+
+    checkPasswordFormat();
+
+    if (!passwordsMatch && passwordConfirm.value.length > 0) {
+        passwordMatchError.classList.remove("hidden");
+    }
+    else {
+        passwordMatchError.classList.add("hidden");
+    }
+
+    if (passwordsMatch && isValid) {
+        submitButton.classList.remove("btn-disabled");
+        submitButton.classList.add("btn-primary");
+        submitButton.removeAttribute("disabled");
+        passwordMatchError.classList.add("hidden");
+    } else {
+        submitButton.classList.add("btn-disabled");
+        submitButton.classList.remove("btn-primary");
+        submitButton.setAttribute("disabled", "true");
     }
 }
