72e620ec9a32dad55da4095ae84dfa0d60322e16
Lucas ESPIET <lespiet@iutbayonne.univ-pau.fr>
2025-01-30 09:39:23 +0100

    :adhesive_bandage: Ajout du support de l'UTF-8 pour la BDD

--- src/App/Db.php
+++ src/App/Db.php
@@ -37,7 +37,8 @@ class Db
     private function __construct()
     {
         try {
-            $this->pdo = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+            $this->pdo = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS,
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo 'Connection failed: ' . $e->getMessage();
