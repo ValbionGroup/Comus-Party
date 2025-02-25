77f6d4c30d21412cc36f81551354cf6d98dd3faf
Lucas ESPIET <lespiet@iutbayonne.univ-pau.fr>
2025-02-25 16:52:43 +0100

    :test_tube: Correction d'un test en échec

--- tests/ControllerAuthTest.php
+++ tests/ControllerAuthTest.php
@@ -35,7 +35,7 @@ class ControllerAuthTest extends TestCase
     public function testAuthenticateOnNullEmail(): void
     {
         $this->assertNotNull($this->controller, 'Controller is null.');
-        $this->assertFalse($this->controller->authenticate(null, 'hashed_password1', ''));
+        $this->assertFalse($this->controller->authenticate(null, 'hashed_password1', 'XXXX.DUMMY.TOKEN.XXXX'));
     }
 
     /**
@@ -46,7 +46,7 @@ class ControllerAuthTest extends TestCase
     public function testAuthenticateOnNullPassword(): void
     {
         $this->assertNotNull($this->controller, 'Controller is null.');
-        $this->assertFalse($this->controller->authenticate('john.doe@example.com', null, ''));
+        $this->assertFalse($this->controller->authenticate('john.doe@example.com', null, 'XXXX.DUMMY.TOKEN.XXXX'));
     }
 
     /**
@@ -56,7 +56,7 @@ class ControllerAuthTest extends TestCase
     public function testAuthenticateOnValidEmailAndPassword(): void
     {
         $this->assertNotNull($this->controller, 'Controller is null.');
-        $this->controller->authenticate('john.doe@example.com', 'hashed_password1', '');
+        $this->controller->authenticate('john.doe@example.com', 'hashed_password1', 'XXXX.DUMMY.TOKEN.XXXX');
     }
 
     /**
@@ -66,7 +66,7 @@ class ControllerAuthTest extends TestCase
     public function testAuthenticateOnInvalidEmailAndValidPassword(): void
     {
         $this->assertNotNull($this->controller, 'Controller is null.');
-        $this->assertFalse($this->controller->authenticate('john.doeexample.com', 'hashed_password1', ''));
+        $this->assertFalse($this->controller->authenticate('john.doeexample.com', 'hashed_password1', 'XXXX.DUMMY.TOKEN.XXXX'));
     }
 
     /**
