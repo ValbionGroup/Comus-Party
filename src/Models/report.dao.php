ca2df0dd8f5668f4824e2291f3e921b06a6b684d
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2025-03-10 19:01:10 +0100

    :sparkles: Ajout de la fonction insert pour report

--- src/Models/report.dao.php
+++ src/Models/report.dao.php
@@ -54,45 +54,24 @@ class ReportDAO
     }
 
     /**
-     * @brief Hydrate un objet Report avec les valeurs du tableau associatif passé en paramètre
-     * @param array $data Le tableau associatif contenant les données à hydrater
-     * @return Report L'objet Report hydraté
-     * @throws DateMalformedStringException
+     * @brief Insère un signalement en base de données
+     * @param Report $report L'objet Report à insérer
+     * @return bool L'objet retourné par la méthode, ici un booléen indiquant si l'insertion a réussi
      */
-    public function hydrate(array $data): Report {
-        $report = new Report();
-        $report->setId($data['id']);
-        $report->setObject(
-            match ($data['object']) {
-                'language' => ReportObject::LANGUAGE,
-                'spam' => ReportObject::SPAM,
-                'links' => ReportObject::LINKS,
-                'fairplay' => ReportObject::FAIRPLAY,
-                default => ReportObject::OTHER
-            }
+    public function insert(Report $report): bool
+    {
+        $stmt = $this->pdo->prepare(
+            'INSERT INTO ' . DB_PREFIX . 'report (object, description, treated_by, reported_uuid, sender_uuid, created_at, updated_at)
+            VALUES (:object, :description, :treated_by, :reported_uuid, :sender_uuid, :created_at, :updated_at)'
         );
-        $report->setDescription($data['description']);
-        $report->setTreatedBy($data['treated_by']);
-        $report->setReportedUuid($data['reported_uuid']);
-        $report->setSenderUuid($data['sender_uuid']);
-        $report->setCreatedAt(new DateTime($data['created_at']));
-        $report->setUpdatedAt(new DateTime($data['updated_at']));
-        return $report;
-    }
-
-    /**
-     * @brief Hydrate un tableau d'objets Report avec les valeurs des tableaux associatifs du tableau passé en paramètre
-     * @details Cette méthode appelle, pour chaque tableau associatif contenu dans celui passé en paramètre, la méthode hydrate() définie ci-dessus.
-     * @param array $data Le tableau de tableaux associatifs
-     * @return array L'objet retourné par la méthode, ici un tableau (d'objets Report)
-     * @throws DateMalformedStringException Exception levée dans le cas d'une date malformée
-    */
-    public function hydrateMany(array $data) {
-        $reports = [];
-        foreach ($data as $row) {
-            $reports[] = $this->hydrate($row);
-        }
-        return $reports;
+        $stmt->bindValue(':object', $report->getObject(), PDO::PARAM_STR);
+        $stmt->bindValue(':description', $report->getDescription(), PDO::PARAM_STR);
+        $stmt->bindValue(':treated_by', $report->getTreatedBy(), PDO::PARAM_STR);
+        $stmt->bindValue(':reported_uuid', $report->getReportedUuid(), PDO::PARAM_STR);
+        $stmt->bindValue(':sender_uuid', $report->getSenderUuid(), PDO::PARAM_STR);
+        $stmt->bindValue(':created_at', $report->getCreatedAt()->format('Y-m-d H:i:s'), PDO::PARAM_STR);
+        $stmt->bindValue(':updated_at', $report->getUpdatedAt()->format('Y-m-d H:i:s'), PDO::PARAM_STR);
+        return $stmt->execute();
     }
 
     /**
@@ -114,6 +93,50 @@ class ReportDAO
             return null;
         }
         return $this->hydrateMany($reportsTab);
+    }
+
+    /**
+     * @brief Hydrate un tableau d'objets Report avec les valeurs des tableaux associatifs du tableau passé en paramètre
+     * @details Cette méthode appelle, pour chaque tableau associatif contenu dans celui passé en paramètre, la méthode hydrate() définie ci-dessus.
+     * @param array $data Le tableau de tableaux associatifs
+     * @return array L'objet retourné par la méthode, ici un tableau (d'objets Report)
+     * @throws DateMalformedStringException Exception levée dans le cas d'une date malformée
+     */
+    public function hydrateMany(array $data)
+    {
+        $reports = [];
+        foreach ($data as $row) {
+            $reports[] = $this->hydrate($row);
+        }
+        return $reports;
+    }
+
+    /**
+     * @brief Hydrate un objet Report avec les valeurs du tableau associatif passé en paramètre
+     * @param array $data Le tableau associatif contenant les données à hydrater
+     * @return Report L'objet Report hydraté
+     * @throws DateMalformedStringException
+     */
+    public function hydrate(array $data): Report
+    {
+        $report = new Report();
+        $report->setId($data['id']);
+        $report->setObject(
+            match ($data['object']) {
+                'language' => ReportObject::LANGUAGE,
+                'spam' => ReportObject::SPAM,
+                'links' => ReportObject::LINKS,
+                'fairplay' => ReportObject::FAIRPLAY,
+                default => ReportObject::OTHER
+            }
+        );
+        $report->setDescription($data['description']);
+        $report->setTreatedBy($data['treated_by']);
+        $report->setReportedUuid($data['reported_uuid']);
+        $report->setSenderUuid($data['sender_uuid']);
+        $report->setCreatedAt(new DateTime($data['created_at']));
+        $report->setUpdatedAt(new DateTime($data['updated_at']));
+        return $report;
     }
 
     /**
