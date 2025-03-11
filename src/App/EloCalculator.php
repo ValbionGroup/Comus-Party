d853e45d557e65e49bfd734d9ddf9d953d90c502
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2025-03-11 10:36:06 +0100

    :memo: Mise à jour de la documentation de EloCalculator.php

--- src/App/EloCalculator.php
+++ src/App/EloCalculator.php
@@ -13,16 +13,20 @@ namespace ComusParty\App;
  */
 class EloCalculator
 {
-    const K_FACTOR = 16;
+    /**
+     * @brief Facteur K
+     * @var int
+     */
+    const int K_FACTOR = 16;
 
     /**
      * @brief Calcule le nouvel Elo du joueur en fonction de son Elo actuel, de l'Elo moyen des joueurs de la partie et du résultat de la partie
      * @param $eloPlayer int Elo du joueur
      * @param $averageElo int Elo moyen des joueurs de la partie
      * @param $result float Résultat de la partie (1 pour une victoire, 0.5 pour un match nul, 0 pour une défaite)
-     * @return int Nouvel Elo du joueur
+     * @return float|int Nouvel Elo du joueur
      */
-    public static function calculateNewElo(int $eloPlayer, int $averageElo, float $result)
+    public static function calculateNewElo(int $eloPlayer, int $averageElo, float $result): float|int
     {
         return $eloPlayer + self::K_FACTOR * ($result - self::getExpectedScore($eloPlayer, $averageElo));
     }
@@ -33,7 +37,7 @@ class EloCalculator
      * @param $averageElo int Elo moyen des joueurs de la partie
      * @return float|int
      */
-    protected static function getExpectedScore(int $eloPlayer, int $averageElo)
+    protected static function getExpectedScore(int $eloPlayer, int $averageElo): float|int
     {
         return 1 / (1 + pow(10, ($averageElo - $eloPlayer) / 400));
     }
