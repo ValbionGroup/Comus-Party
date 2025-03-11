883b696f3c9c2be286133ed36ba9abc0b701c35b
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2025-03-11 10:25:13 +0100

    :memo: Mise à jour de la documentation de Dashboard.php

--- src/App/Sockets/Dashboard.php
+++ src/App/Sockets/Dashboard.php
@@ -17,33 +17,57 @@ use SplObjectStorage;
 
 /**
  * @brief Classe Dashboard (Sockets)
+ * @details Classe permettant de gérer le dashboard en temps réel
  */
 class Dashboard implements MessageComponentInterface
 {
+    /**
+     * @brief Liste des clients connectés
+     * @var SplObjectStorage
+     */
     protected SplObjectStorage $clients;
 
+    /**
+     * @brief Constructeur de la classe Dashboard
+     */
     public function __construct()
     {
         $this->clients = new SplObjectStorage;
     }
 
+    /**
+     * @brief Fonction appelée lors de la connexion d'un modérateur
+     * @details Ajoute le modérateur à la liste des clients
+     * @param ConnectionInterface $conn La connexion du joueur
+     */
     function onOpen(ConnectionInterface $conn): void
     {
         $conn->send(json_encode(['message' => 'Connection established']));
         $this->clients->attach($conn);
     }
 
+    /**
+     * @brief Fonction appelée lors de la déconnexion d'un modérateur
+     * @details Supprime le modérateur de la liste des clients
+     * @param ConnectionInterface $conn La connexion du joueur
+     */
     function onClose(ConnectionInterface $conn): void
     {
         $this->clients->detach($conn);
     }
 
-    function onError(ConnectionInterface $conn, Exception $e)
+    function onError(ConnectionInterface $conn, Exception $e): void
     {
         echo "An error has occurred: {$e->getMessage()}\n";
         $conn->close();
     }
 
+    /**
+     * @brief Fonction appelée lors de la réception d'un message
+     * @details Envoie le message reçu à tous les clients connectés
+     * @param ConnectionInterface $from La connexion du joueur
+     * @param string $msg Le message reçu
+     */
     function onMessage(ConnectionInterface $from, $msg): void
     {
         $command = json_decode($msg, true)['command'];
