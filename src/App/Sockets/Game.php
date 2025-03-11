c697868b6c0cbf47e940f25fefef3e63c44c0fbb
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2025-03-11 10:33:00 +0100

    :memo: Mise à jour de la documentation de Game.php

--- src/App/Sockets/Game.php
+++ src/App/Sockets/Game.php
@@ -20,23 +20,48 @@ use SplObjectStorage;
 
 /**
  * @brief Classe Game (Sockets)
+ * @details Classe permettant de gérer le jeu en temps réel
  */
 class Game implements MessageComponentInterface
 {
+    /**
+     * @brief Liste des clients connectés
+     * @var SplObjectStorage
+     */
     protected SplObjectStorage $clients;
+
+    /**
+     * @brief Liste des parties en cours
+     * @var array
+     */
     protected array $games;
 
+    /**
+     * @brief Constructeur de la classe Game
+     */
     public function __construct()
     {
         $this->clients = new SplObjectStorage;
         $this->games = [];
     }
 
+    /**
+     * @brief Fonction appelée lors de la connexion d'un joueur
+     * @details Envoie un message de confirmation de connexion
+     * @param ConnectionInterface $conn La connexion du joueur
+     */
     function onOpen(ConnectionInterface $conn): void
     {
         $conn->send(json_encode(['message' => 'Connection established']));
     }
 
+    /**
+     * @brief Fonction appelée lors de la réception d'un message
+     * @details Gère l'affichage des joueurs en temps réel
+     * @param ConnectionInterface $from La connexion du joueur
+     * @param string $msg Le message reçu
+     * @throws Exception
+     */
     function onMessage(ConnectionInterface $from, $msg): void
     {
         $data = json_decode($msg, true);
@@ -71,6 +96,12 @@ class Game implements MessageComponentInterface
         }
     }
 
+    /**
+     * @brief Fonction appelée lors de l'arrivée et la déconnexion d'un joueur
+     * @details Met à jour la liste des joueurs
+     * @param string $game
+     * @throws Exception
+     */
     private function updatePlayer(string $game): void
     {
         $gameRecord = (new GameRecordDAO(Db::getInstance()->getConnection()))->findByCode($game);
@@ -89,6 +120,12 @@ class Game implements MessageComponentInterface
         $this->sendToGame($game, "updatePlayer", json_encode($jsonPlayer));
     }
 
+    /**
+     * @brief Fonction permettant d'envoyer un message à une partie
+     * @param string $game La partie à laquelle envoyer le message
+     * @param string $command La commande à envoyer
+     * @param string $content Le contenu du message
+     */
     private function sendToGame(string $game, string $command, string $content): void
     {
         foreach ($this->games[$game] as $client) {
@@ -96,6 +133,13 @@ class Game implements MessageComponentInterface
         }
     }
 
+    /**
+     * @brief Fonction appelée lorsque la partie est démarrée
+     * @details Redirige les joueurs vers la partie
+     * @param string $game
+     * @param string $uuid
+     * @throws Exception
+     */
     private function redirectUserToGame(string $game, string $uuid): void
     {
         $gameRecord = (new GameRecordDAO(Db::getInstance()->getConnection()))->findByCode($game);
@@ -106,6 +150,12 @@ class Game implements MessageComponentInterface
         }
     }
 
+    /**
+     * @brief Fonction appelée lors de la déconnexion d'un joueur
+     * @details Retire le joueur de la liste des clients
+     * @param ConnectionInterface $conn La connexion du joueur
+     * @throws Exception
+     */
     public function onClose(ConnectionInterface $conn): void
     {
         foreach ($this->games as $game => $clients) {
@@ -119,6 +169,11 @@ class Game implements MessageComponentInterface
         $this->clients->detach($conn);
     }
 
+    /**
+     * @brief Fonction appelée lors d'une erreur
+     * @param ConnectionInterface $conn La connexion du joueur
+     * @param Exception $e L'exception
+     */
     public function onError(ConnectionInterface $conn, Exception $e): void
     {
         echo "Erreur: {$e->getMessage()}\n";
@@ -126,8 +181,9 @@ class Game implements MessageComponentInterface
     }
 
     /**
-     * @param string $string The string to escape
-     * @return string The escaped string
+     * @brief Fonction permettant d'échapper les caractères spéciaux
+     * @param string $string La chaîne à échapper
+     * @return string La chaîne échappée
      */
     protected function escape(string $string): string
     {
