10246e1e497c2f5e3704063eeae29bf766879149
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2025-03-11 10:15:49 +0100

    :art: Utilisation de la méthode `makeRequest` sur `denySuggest()`

--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -58,25 +58,16 @@ function showModalSuggest(e) {
 
 function denySuggest(e) {
     let id = e.parentNode.children[0].value;
-    const xhr = new XMLHttpRequest();
-    xhr.open("PUT", `/suggest/deny/${id}`, true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    // Envoyer les données sous forme de paire clé=valeur
-    xhr.send();
-
-    // Gérer la réponse du serveur
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let response = JSON.parse(xhr.responseText);
-            if (response.success) {
-                closeModal();
-                dashboardConnection.send(JSON.stringify({command: 'updateSuggests'}));
-                showNotification("Génial !", "La suggestion a bien été refusée", "green");
-            } else {
-                showNotification("Oups...", "La suggestion n'a pas pu être refusée", "red");
-            }
+    makeRequest('PUT', `/suggest/deny/${id}`, (response) => {
+        response = JSON.parse(response);
+        if (response.success) {
+            closeModal();
+            dashboardConnection.send(JSON.stringify({command: 'updateSuggests'}));
+            showNotification("Génial !", "La suggestion a bien été refusée", "green");
+        } else {
+            showNotification("Oups...", "La suggestion n'a pas pu être refusée", "red");
         }
-    };
+    });
 }
 
 function acceptSuggest(e) {
